1a24c38420dd9e066c59876bfa77f3e6ce5dc05c
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2023-10-10 14:19:43 -0500

    Declaracion de atributos y constructores reserva y persona

--- src/com/sam/sistemaReservas/persona/Persona.java
+++ src/com/sam/sistemaReservas/persona/Persona.java
@@ -6,4 +6,41 @@ public class Persona {
     private Long numeroDocumento;
     private Integer numeroContacto;
 
+    public Persona(String nombre, Long numeroDocumento, Integer numeroContacto) {
+        this.nombre = nombre;
+        this.numeroDocumento = numeroDocumento;
+        this.numeroContacto = numeroContacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(Long numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public Integer getNumeroContacto() {
+        return numeroContacto;
+    }
+
+    public void setNumeroContacto(Integer numeroContacto) {
+        this.numeroContacto = numeroContacto;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona" +
+                "nombre='" + nombre + '\n' +
+                ", numeroDocumento=" + numeroDocumento +
+                ", \nnumeroContacto=" + numeroContacto ;
+    }
 }
--- src/com/sam/sistemaReservas/reserva/Reserva.java
+++ src/com/sam/sistemaReservas/reserva/Reserva.java
@@ -4,18 +4,50 @@ import com.sam.sistemaReservas.persona.Persona;
 
 import java.time.LocalDate;
 
-public class Reserva {
+public abstract class Reserva {
 
-    private static int contadorID = 0;
+    private static int contadorId = 0;
     private Integer id;
     private LocalDate fechaHoraReserva;
 
     private String queEstaReservando;
     private Persona persona;
 
+    public Reserva(LocalDate fechaHoraReserva, String queEstaReservando, Persona persona) {
+        this.id = ++contadorId;
+        this.fechaHoraReserva = LocalDate.now();
+        this.queEstaReservando = queEstaReservando;
+        this.persona = persona;
+    }
 
+    public LocalDate getFechaHoraReserva() {
+        return fechaHoraReserva;
+    }
 
+    public void setFechaHoraReserva(LocalDate fechaHoraReserva) {
+        this.fechaHoraReserva = fechaHoraReserva;
+    }
 
+    public String getQueEstaReservando() {
+        return queEstaReservando;
+    }
 
+    public void setQueEstaReservando(String queEstaReservando) {
+        this.queEstaReservando = queEstaReservando;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+/*    public void verDatosReserva() {
+
+        System.out.println("Estos son los datos de tu reserva: \n Reserva a nombre de: "+ getPersona().getNombre() + "\nNumero documento: " + getPersona().getNumeroDocumento() +
+                "\n " );
+    }*/
 
 }
