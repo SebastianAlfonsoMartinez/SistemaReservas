1ec089364d8981d34f9b65f6947f00a2ce1daa29
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2023-10-10 13:26:55 -0500

    Creacion proyecto de sistema de reservas

---
+++ src/com/sam/sistemaReservas/persona/Persona.java
@@ -0,0 +1,9 @@
+package com.sam.sistemaReservas.persona;
+
+public class Persona {
+
+    private String nombre;
+    private Long numeroDocumento;
+    private Integer numeroContacto;
+
+}
---
+++ src/com/sam/sistemaReservas/reserva/Reserva.java
@@ -0,0 +1,21 @@
+package com.sam.sistemaReservas.reserva;
+
+import com.sam.sistemaReservas.persona.Persona;
+
+import java.time.LocalDate;
+
+public class Reserva {
+
+    private static int contadorID = 0;
+    private Integer id;
+    private LocalDate fechaHoraReserva;
+
+    private String queEstaReservando;
+    private Persona persona;
+
+
+
+
+
+
+}
